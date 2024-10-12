8fb7ee68e49746cc3aacc61601d99ccefe4d603d
Arash Zarghami <eng.ar.zarghami@gmail.com>
2024-10-12 17:23:36 +0330

    all changes

--- src/components/infoCard/InfoCard.tsx
+++ src/components/infoCard/InfoCard.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 const InfoCard = ({ name, code, handleCopy }: Props) => {
   return (
-    <div className="bg-[rgba(250,250,250,0.5)] w-full h-[20%] rounded-lg flex justify-between p-5">
+    <div className="bg-slate-400 w-full h-[20%] rounded-lg flex justify-between p-5">
       <div className="flex justify-center items-center gap-3 ">
         <p>Name:</p>
         <div className="flex justify-center items-center bg-black w-fit p-5 h-2 rounded-xl text-white font-sans text-xl ">
@@ -17,7 +17,7 @@ const InfoCard = ({ name, code, handleCopy }: Props) => {
         <p>Code:</p>
         <div
           onClick={() => handleCopy(code)}
-          className="flex justify-center items-center bg-blue-400 w-fit p-5 h-2 rounded-xl text-white font-sans text-xl cursor-pointer hover:scale-105 transition-all "
+          className="flex justify-center items-center bg-blue-500 w-fit p-5 h-2 rounded-xl text-white font-sans text-xl cursor-pointer hover:scale-105 transition-all "
         >
           {code}
         </div>
